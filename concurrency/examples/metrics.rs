0267061a14b1590b963ee95c77761fd5e2481155
Zixin Zhou <zhouzixin@apache.org>
2024-10-05 15:53:39 +0800

    Add support rwlock and display

--- concurrency/examples/metrics.rs
+++ concurrency/examples/metrics.rs
@@ -31,32 +31,42 @@ fn main() -> Result<()> {
     println!("{:?}", metrics.snapshot());
 
     for idx in 0..N {
-        task_worker(idx, metrics.clone());
+        task_worker(idx, metrics.clone())?;
     }
 
     for _ in 0..M {
-        request_worker(metrics.clone());
+        request_worker(metrics.clone())?;
     }
 
     loop {
         thread::sleep(Duration::from_secs(2));
-        println!("{:?}", metrics.snapshot());
+        println!("{}", metrics);
     }
 }
 
-fn task_worker(idx: usize, metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-        metrics.inc(format!("call.thread.worker.{}", idx)).unwrap();
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
 
-fn request_worker(metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-        let page = rng.gen_range(1..5);
-        metrics.inc(format!("req.page.{}", page)).unwrap();
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..5);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
--- concurrency/src/metrics/metrics_default.rs
+++ concurrency/src/metrics/metrics_default.rs
@@ -20,30 +20,42 @@
 
 use anyhow::Result;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::fmt::Display;
+use std::sync::{Arc, RwLock};
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<Mutex<HashMap<String, i64>>>,
+    data: Arc<RwLock<HashMap<String, i64>>>,
 }
 
 impl Metrics {
     pub fn new() -> Metrics {
         Metrics {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock()
+        let mut data = self.data.write()
             .map_err(|e| anyhow::anyhow!(e.to_string()))?;
         let counter = data.entry(key.into()).or_insert(0);
         *counter += 1;
         Ok(())
     }
 
+    #[allow(dead_code)]
     pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        let data = self.data.lock().map_err(|e| anyhow::anyhow!(e.to_string()))?;
+        let data = self.data.read().map_err(|e| anyhow::anyhow!(e.to_string()))?;
         Ok(data.clone())
+    }
+}
+
+impl Display for Metrics {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let data = self.data.read().map_err(|_e| std::fmt::Error {})?;
+        for (key, value) in data.iter() {
+            writeln!(f, "{}: {}", key, value)?;
+        }
+        Ok(())
     }
 }

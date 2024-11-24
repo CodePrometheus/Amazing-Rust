c37e6098be583571cefe9c01e51077582805deb3
Zixin Zhou <zhouzixin@apache.org>
2024-11-24 10:24:57 +0800

    Add ecosystem project

---
+++ ecosystem/examples/err.rs
@@ -0,0 +1,70 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::fs;
+use std::num::ParseIntError;
+use anyhow::Context;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Parse error: {0}")]
+    Parse(#[from] ParseIntError),
+    #[error("Serialize json error: {0}")]
+    Serialize(#[from] serde_json::Error),
+
+    #[error("Error: {0:?}")]
+    BigError(Box<BigError>),
+
+    #[error("Custom error: {0}")]
+    Custom(String),
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub struct BigError {
+    a: String,
+    b: Vec<String>,
+    c: [u8; 64],
+    d: u64,
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    println!("size of anyhow::Error is {}", size_of::<anyhow::Error>());
+    println!("size of std::io::Error is {}", size_of::<std::io::Error>());
+    println!(
+        "size of std::num::ParseIntError is {}",
+        size_of::<ParseIntError>()
+    );
+    println!(
+        "size of serde_json::Error is {}",
+        size_of::<serde_json::Error>()
+    );
+    println!("size of string is {}", size_of::<String>()); // 24
+    println!("size of MyError is {}", size_of::<MyError>());
+
+    let filename = "non-existent-file.txt";
+    let _fd = fs::File::open(filename).with_context(|| format!("Can not find file: {}", filename))?;
+    fail_with_error()?;
+    Ok(())
+}
